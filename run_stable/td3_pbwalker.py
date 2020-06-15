8430867b0f0db1581a608f4def4c21490d242e42
sgillen <sgillen@ucsb.edu>
2020-06-15 00:17:54 -0700

    record videos, some meshing analysis

--- run_stable/td3_pbwalker.py
+++ run_stable/td3_pbwalker.py
@@ -14,13 +14,10 @@ import json
 import pybulletgym
 import pybulletgym.envs.mujoco.envs.locomotion.walker2d_env
 from seagul.envs.wrappers.pybullet_physics import PyBulletPhysicsWrapper
+import pybullet as p
+num_steps = int(2e6)
 
-
-
-num_steps = int(1e6)
-
-
-base_dir = "./data_pbp/td3"
+base_dir = "./data_pbp/td3/"
 trial_name = input("Trial name: ")
 
 trial_dir = base_dir + trial_name + "/"
@@ -29,8 +26,70 @@ base_ok = input("run will be saved in " + trial_dir + " ok? y/n")
 if base_ok == "n":
     exit()
 
+# physics_params = {
+#     'fixedTimeStep': 0.008,
+#     'numSubSteps': 4,
+#     'numSolverIterations': 5,
+#     'useSplitImpulse': 1,
+#     'splitImpulsePenetrationThreshold': -0.03999999910593033,
+#     'contactBreakingThreshold': 0.02,
+#     'collisionFilterMode': 1,
+#     'enableFileCaching': 1,
+#     'restitutionVelocityThreshold': 0.20000000298023224,
+#     'erp': 0.0,
+#     'frictionERP': 0.0,
+#     'contactERP': 0.0,
+#     'globalCFM': 0.0,
+#     'enableConeFriction': 0,
+#     'deterministicOverlappingPairs': 1,
+#     'allowedCcdPenetration': 0.04,
+#     'jointFeedbackMode': 0,
+#     'solverResidualThreshold': 1e-07,
+#     'contactSlop': 1e-05,
+#     'enableSAT': 0,
+#     'constraintSolverType': 0,
+#     'reportSolverAnalytics': 1,
+# }
 
-env_config={}
+# dynamics_params = {
+#     'lateralFriction': 0.8,
+#     'rollingFriction': 0.1,
+#     'spinningFriction': 0.1,
+#     'contactStiffness': -1,
+#     'contactDamping': -1,
+#     'restitution': .325,
+#     #'contactStiffness': 23201.60208068,
+#     #'contactDamping':  948.95018837,
+#     #'restitution': 0.0,
+#     #'contactStiffness': 2493.074792243767,
+#     #'contactDamping': 105.26315789473685 ,
+#     #'restitution': 0.0,
+#     'collisionMargin': 0.0,
+#     'angularDamping': 0.0,
+#     'linearDamping': 0.0,
+#     'jointDamping': .1,
+# }
+
+
+
+physics_params = {
+    'fixedTimeStep': 0.008,
+    'numSubSteps': 4,
+    'numSolverIterations': 5,
+    #'constraintSolverType': p.CONSTRAINT_SOLVER_LCP_DANTZIG,
+    'globalCFM': 0.000001,
+    'solverResidualThreshold': 0.001,
+    'numSolverIterations': 200
+}
+
+dynamics_params = {
+    'lateralFriction': 0.8,
+    'rollingFriction': 0.1,
+    'spinningFriction': 0.1,
+}
+
+env_config = {'physics_params':physics_params, 'dynamics_params':dynamics_params}
+env_kwargs = {'env':gym.make('Walker2DMuJoCoEnv-v0'), 'physics_params':physics_params, 'dynamics_params':dynamics_params}
 
 def run_stable(num_steps, save_dir):
 
@@ -39,47 +98,9 @@ def run_stable(num_steps, save_dir):
 
     # env = make_vec_env(pybulletgym.envs.mujoco.envs.locomotion.walker2d_env.Walker2DMuJoCoEnv, n_envs=1, monitor_dir=save_dir, env_kwargs=env_config)
 
-        physics_params = {
-        'fixedTimeStep': 0.008,
-        'numSubSteps': 4,
-        'numSolverIterations': 5,
-        'useSplitImpulse': 1,
-        'splitImpulsePenetrationThreshold': -0.03999999910593033,
-        'contactBreakingThreshold': 0.02,
-        'collisionFilterMode': 1,
-        'enableFileCaching': 1,
-        'restitutionVelocityThreshold': 0.20000000298023224,
-        'erp': 0.0,
-        'frictionERP': 0.0,
-        'contactERP': 0.0,
-        'globalCFM': 0.0,
-        'enableConeFriction': 0,
-        'deterministicOverlappingPairs': 1,
-        'allowedCcdPenetration': 0.04,
-        'jointFeedbackMode': 0,
-        'solverResidualThreshold': 1e-07,
-        'contactSlop': 1e-05,
-        'enableSAT': 0,
-        'constraintSolverType': 0,
-        'reportSolverAnalytics': 1,
-    }
-
-    dynamics_params = {
-        'lateralFriction': 0.7,
-        'restitution': 0.325,
-        'rollingFriction': 0.1,
-        'spinningFriction': 0.1,
-        'contactDamping': -1.0,
-        'contactStiffness': -1.0,
-        'collisionMargin': 0.0,
-        'angularDamping': 0.0,
-        'linearDamping': 0.0,
-        'jointDamping': .1,
-    }
-
     
     env = PyBulletPhysicsWrapper
-    env_kwargs = {'env': gym.make("Walker2DMuJoCoEnv-v0"), 'physics_params':{},'dynamics_params':{}}
+
     env = make_vec_env(env, n_envs=1, monitor_dir=save_dir, env_kwargs=env_kwargs)
 
     n_actions = env.action_space.shape[-1]
